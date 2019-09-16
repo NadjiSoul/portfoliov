5517ed6435ca4f09aedffaa372d37ba1d034aeff
Soulaimana Nadji <nadji.soulaimana@gmail.com>
2019-09-16 17:27:18 +0200

    upgrade portfolio repository

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<title>My New Portfolio</title>
+</head>
+<body>
+	<header>
+		<h2>WEB DEVELOPER JUNIOR</h2>
+		<h1>SOULAIMANA NADJI</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Project</a></li>
+			<li><a href="#">Creation</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+<script type="text/javascript" src="./js/script.js"></script>
+</body>
+</html>
